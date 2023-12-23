9450163916a67471ad976231d21a6af1febb16b9
Delcoro, Jofli Asaph <asaphdelcoro987@gmail.com>
2023-12-23 09:52:19 +0800

    added Getbanks test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import unittest
 import warnings
 from api import app
 
-class MyAppTests(unnittest.TestCase):
+class MyAppTests(unittest.TestCase):
     def setUp(self):
         app.config["TESTING"] = True
         self.app = app.test_client()
@@ -10,7 +10,12 @@ class MyAppTests(unnittest.TestCase):
     def test_index_page(self):
         response = self.app.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode(), "<p>Hello, World!</p>")
+        self.assertEqual(response.data.decode(), 'Index Page')
+        
+    def test_GetBanks(self):
+        response = self.app.get("/banks")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("BDO" in response.data.decode())
         
 if __name__ == "__main__":
     unittest.main()
