2ce805b68f6190db987795e5952eb932d848ab0c
Delcoro, Jofli Asaph <asaphdelcoro987@gmail.com>
2023-12-23 09:17:12 +0800

    installed flask_mysqldb

--- api.py
+++ api.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 from markupsafe import escape
 
 app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "root"
+app.config["MYSQL_DB"] = "loan_management"
+
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
 
 @app.route("/")
 def index():
